317d2c5d3f1d46d71721fb72b9788d61da3c3e84
farid <faridasfani6@gmail.com>
2026-09-20 19:10:40 +0700

    add  class kerucut dan tambah isi class kerucut (farid)

---
+++ src/tk2/kelompok3/KerucutFarid.java
@@ -0,0 +1,46 @@
+package tk2.kelompok3;
+public class KerucutFarid {
+    
+    // Class Kerucut
+public class Kerucut {
+    
+    // Atribut
+    private double jariJari, tinggi, garisPelukis;
+
+    // Construcktor
+    public Kerucut(double jariJari, double tinggi, double garisPelukis) {
+        this.jariJari = jariJari;
+        this.tinggi = tinggi;
+        this.garisPelukis = garisPelukis;
+    }
+
+    // Setter & Getter
+    public void setJariJari(double r) { this.jariJari = r; }
+    public double getJariJari() { return jariJari; }
+
+    public void setTinggi(double t) { this.tinggi = t; }
+    public double getTinggi() { return tinggi; }
+
+    public void setGarisPelukis(double s) { this.garisPelukis = s; }
+    public double getGarisPelukis() { return garisPelukis; }
+
+    // Method hitung (return)
+    public double volume() {
+        return (1.0/3.0) * Math.PI * Math.pow(jariJari, 2) * tinggi;
+    }
+    public double luasPermukaan() {
+        return Math.PI * jariJari * (jariJari + garisPelukis);
+    }
+
+    // Method tampil
+    public void tampil() {
+        System.out.println("Rumus Volume (V) = 1/3 * phi * r * r * t");
+        System.out.println("Rumus Luas Permukaan (L) = phi * r * (r + s)");
+        System.out.println("Diketahui : r = " + (int) jariJari + "\nt = " + (int) tinggi + "\ns = " + (int) garisPelukis + "\nphi = " + String.format("%.2f", Math.PI));
+        System.out.println("Ditanya : V, L?");
+        System.out.printf("V = %.2f%n", volume());
+        System.out.printf("L = %.2f%n", luasPermukaan());
+        System.out.println();
+    }
+}
+}
