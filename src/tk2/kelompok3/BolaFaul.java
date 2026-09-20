163d1a8fd5db248b30f686e190cb00d24620942f
syifaul3300 <akun.saya3300@gmail.com>
2026-09-20 15:19:24 +0700

    add bola faul

---
+++ src/tk2/kelompok3/BolaFaul.java
@@ -0,0 +1,29 @@
+package tk2.kelompok3;
+
+public class BolaFaul {
+    private double jariJari;
+
+    public double getJariJari() {
+        return jariJari;
+    }
+
+    public void setJariJari(double jariJari) {
+        this.jariJari = jariJari;
+    }
+    public double volume(){
+        return (4.0/3.0) * Math.PI * Math.pow(jariJari, 3);
+    }
+    public double LuasPermukaan(){
+        return 4 * Math.PI * Math.pow(jariJari, 2);
+    }
+    public void tampil(){
+        System.out.println("Rumus Volume (V) = 4/3 * phi * r * r * r");
+        System.out.println("Rumus Luas Permukaan (L) = 4 * phi * r * r");
+        System.out.println("Diketahui : r = " + (int) jariJari
+                         + ", phi = " + String.format("%.2f", Math.PI));
+        System.out.println("Ditanya : V, L?");
+        System.out.printf("V = %.2f%n", volume());
+        System.out.printf("L = %.2f%n" , LuasPermukaan());
+        System.out.println();
+    }
+}
