4d54187f6f58828a09fc060f7b66369dc69ad3b0
bagastama <afiftama04@gmail.com>
2026-09-20 14:31:53 +0700

    Add Class Balok & Mengisi Atribute Setter Getter (Bagas Ganteng)

---
+++ src/tk2/kelompok3/BalokBagas.java
@@ -0,0 +1,48 @@
+package tk2.kelompok3;
+
+public class BalokBagas {
+    
+    private double panjang;
+    private double lebar;
+    private double tinggi;
+
+    public double getPanjang() {
+        return panjang;
+    }
+
+    public void setPanjang(double panjang) {
+        this.panjang = panjang;
+    }
+
+    public double getLebar() {
+        return lebar;
+    }
+
+    public void setLebar(double lebar) {
+        this.lebar = lebar;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+    public double volume(){
+        return panjang*lebar*tinggi;
+    }
+    public double LuasPermukaaan(){
+        return 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi));
+    }
+    public void tampil(){
+        System.out.println("Rumus Volume (V2) = p * l * t");
+        System.out.println("Rumus Luas Permukaan (L) = 2 * (p*1 + p*t + 1*t)");
+        System.out.println("Diketahui : p = " + (int)panjang + "\nl = " + (int)lebar + "\nt = " + (int)tinggi );
+        System.out.println("Ditanya : V, L?");
+        System.out.println("V = " + volume());
+        System.out.println("L = " + LuasPermukaaan());
+        System.out.println();
+       
+    }
+}
