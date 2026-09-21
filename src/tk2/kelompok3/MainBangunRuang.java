19205474f2dfebdcdd6e02fd5e9f3a4a18c04d0b
natasza <natazaputri22@gmail.com>
2026-09-21 16:57:05 +0700

    menambahkan rumus tabung di class main

--- src/tk2/kelompok3/MainBangunRuang.java
+++ src/tk2/kelompok3/MainBangunRuang.java
@@ -2,9 +2,12 @@ package tk2.kelompok3;
 public class MainBangunRuang {
     public static void main(String[] args) {
         // TODO code application logic here
-        // ===== 1. KUBUS (Anggota 1) =====
+        // 1. KUBUS 
         KubusBetrin kubus = new KubusBetrin(14);
         kubus.tampil();
+        // 2.Tabung
+        TabungNatasza tabung = new TabungNatasza(7, 10);
+        tabung.tampil();
     }
     
 }
