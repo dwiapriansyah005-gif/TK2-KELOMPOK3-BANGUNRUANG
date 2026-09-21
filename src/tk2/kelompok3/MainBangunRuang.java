20c8eac9bf7b5cbebcec3046408c7ff63732b89b
betrindina <betrindina@gmail.com>
2026-09-21 16:50:26 +0700

    mengisi class main dengan rumus kubus

--- src/tk2/kelompok3/MainBangunRuang.java
+++ src/tk2/kelompok3/MainBangunRuang.java
@@ -1,21 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tk2.kelompok3;
-
-/**
- *
- * @author macbookairm12020
- */
 public class MainBangunRuang {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        // ===== 1. KUBUS (Anggota 1) =====
+        KubusBetrin kubus = new KubusBetrin(14);
+        kubus.tampil();
     }
     
 }
