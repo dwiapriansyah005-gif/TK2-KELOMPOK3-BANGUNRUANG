c76929c9f718163cd4d665e039345829c75d8058
dwi <dwiapriansyah005@gmail.com>
2026-09-26 15:31:56 +0700

    add jframe

--- src/tk2/kelompok3/MainBangunRuang.java
+++ src/tk2/kelompok3/MainBangunRuang.java
@@ -1,5 +1,7 @@
 package tk2.kelompok3;
+
 public class MainBangunRuang {
+
     public static void main(String[] args) {
         // TODO code application logic here
         // 1. KUBUS 
@@ -9,17 +11,32 @@ public class MainBangunRuang {
         TabungNatasza tabung = new TabungNatasza(7, 10);
         tabung.tampil();
         //3.Balok
-        BalokBagas balok = new BalokBagas(8,4,3);
+        BalokBagas balok = new BalokBagas(8, 4, 3);
         balok.tampil();
         //4.Bola
         BolaFaul bola = new BolaFaul(7);
         bola.tampil();
         //5.Kerucut
-        KerucutFarid kerucut = new KerucutFarid(7,24,25);
+        KerucutFarid kerucut = new KerucutFarid(7, 24, 25);
         kerucut.tampil();
         //6.Limas Segi Empat
-        DwiLimassegiempat limas = new DwiLimassegiempat (10, 5, 12);
-        limas.tampil();
+        DwiLimassegiempat limas = new DwiLimassegiempat(10, 5, 12);
+
+        limas.volume();
+        limas.luasPermukaan();
+
+        double v = Math.round(limas.getVolume() * 100.0) / 100.0;
+        double l = Math.round(limas.getLuasPermukaan() * 100.0) / 100.0;
+
+        System.out.println("------Limas Segi Empat------");
+        System.out.println("Rumus Volume (V) = 1/3 * s * s * t");
+        System.out.println("Rumus Luas Permukaan (L) = (s*s) + 4*(1/2 * s * tinggiSisi)");
+        System.out.println("Diketahui : s = " + (int) limas.getSisiAlas());
+        System.out.println("Diketahui : t = " + (int) limas.getTinggiLimas());
+        System.out.println("Diketahui : tinggiSisi = " + (int) limas.getTinggiSisi());
+
+        System.out.println("Volume = " + v);
+        System.out.println("Luas Permukaan = " + l);
     }
-    
+
 }
